8bb19f36c86af2356e60271c6e6a2ae5076679aa
Alongedaniel <alongedaniel41@gmail.com>
2024-03-20 15:51:04 +0100

    done

--- src/Firebase/FirebaseAuth.js
+++ src/Firebase/FirebaseAuth.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import "firebase/auth";
 
 const firebaseConfig = {
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
